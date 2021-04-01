75e4a02f5b84a85a5f1e8dfa55976f564d918fb4
Felipe Leal <felipewanderson2020@gmail.com>
2021-04-01 11:20:24 -0300

    melhorando a logica de atualização do estado no local storage

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
+import { createContext, ReactNode, useContext, useEffect, useRef, useState } from 'react';
 import { toast } from 'react-toastify';
 import { api } from '../services/api';
 import { Product, Stock } from '../types';
@@ -32,6 +32,19 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     return [];
   });
 
+  const cartPrevRef = useRef<Product[]>();
+
+  useEffect(() => {
+    cartPrevRef.current = cart;
+  });
+
+  const cartPreviousValue = cartPrevRef.current ?? cart;
+
+  useEffect(() =>{
+    if(cartPreviousValue !== cart) {
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(cart));
+    }
+  }, [cartPreviousValue, cart]);
   const addProduct = async (productId: number) => {
     try {
       const updatedCart = [...cart];
@@ -63,7 +76,6 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
       }
 
       setCart(updatedCart);
-      localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart));
     } catch {
       toast.error('Erro na adição do produto');
     }
@@ -79,7 +91,6 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
         toast.error('Erro na remoção do produto');
         return;
       }
-      localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart));
       setCart(updatedCart);
     } catch {
       toast.error('Erro na remoção do produto');
@@ -106,7 +117,6 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
       const productExist = updatedCart.find(product => product.id === productId);
       if(productExist) {
         productExist.amount = amount;
-        localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart));
         setCart(updatedCart);
       }else {
         throw Error();  
